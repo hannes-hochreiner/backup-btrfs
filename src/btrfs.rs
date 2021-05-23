be52f4e952728fa4f9740de638492432baae1dda
Hannes Hochreiner <hannes@hochreiner.net>
2021-05-23 10:35:06 +0200

    extract tests

--- src/btrfs.rs
+++ src/btrfs.rs
@@ -3,6 +3,8 @@ use crate::command::{Command, CommandSystem, Context};
 use anyhow::{Result, Error, anyhow};
 use chrono::{SecondsFormat, Utc};
 use uuid::Uuid;
+#[cfg(test)]
+mod tests;
 
 pub struct Btrfs {
     command: Box<dyn Command>,
@@ -133,93 +135,5 @@ impl Btrfs {
         }
 
         Ok(subvolumes)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use std::str::FromStr;
-    use uuid::Uuid;
-    use crate::{btrfs::{Btrfs, Subvolume}, command::{CommandMock, Context}};
-
-    #[test]
-    fn delete_subvolume() {
-        let mut btrfs = Btrfs {
-            command: Box::new(CommandMock {
-                commands: vec![("sudo btrfs subvolume delete \"/tmp\"".into(), Context::Local {user: "test".into()})],
-                responses: vec![String::new()]
-            })
-        };
-
-        assert!(btrfs.delete_subvolume(&String::from("/tmp"), &Context::Local {user: "test".into()}).is_ok());
-    }
-
-    #[test]
-    fn delete_subvolume_home() {
-        let mut btrfs = Btrfs {
-            command: Box::new(CommandMock {
-                commands: vec![("sudo btrfs subvolume delete \"/home\"".into(), Context::Local {user: "test".into()})],
-                responses: vec![String::new()]
-            })
-        };
-
-        assert!(btrfs.delete_subvolume(&String::from("/home"), &Context::Local {user: "test".into()}).is_err());
-    }
-
-    #[test]
-    fn delete_subvolume_root() {
-        let mut btrfs = Btrfs {
-            command: Box::new(CommandMock {
-                commands: vec![("sudo btrfs subvolume delete \"/\"".into(), Context::Local {user: "test".into()})],
-                responses: vec![String::new()]
-            })
-        };
-
-        assert!(btrfs.delete_subvolume(&String::from("/"), &Context::Local {user: "test".into()}).is_err());
-    }
-
-    #[test]
-    fn get_local_subvolumes() {
-        let mut btrfs = Btrfs { command: Box::new(CommandMock {
-            commands: vec![(String::from("sudo btrfs subvolume list -tupqR --sort=rootid /"), Context::Local {user: "test".into()})],
-            responses: vec![String::from(r#"ID      gen     parent  top level       parent_uuid     received_uuid   uuid    path
---      ---     ------  ---------       -----------     -------------   ----    ----
-256     119496  5       5               -                                       -                                       11eed410-7829-744e-8288-35c21d278f8e    home
-359     119496  5       5               -                                       -                                       32c672fa-d3ce-0b4e-8eaa-ab9205f377ca    root
-360     119446  359     359             -                                       -                                       5f0b151b-52e4-4445-aa94-d07056733a1f    opt/btrfs_test
-367     118687  359     359             5f0b151b-52e4-4445-aa94-d07056733a1f    -                                       7f305e3e-851b-974b-a476-e2f206e7a407    snapshots/2021-05-02T07:40:32Z_inf_btrfs_test
-370     119446  359     359             5f0b151b-52e4-4445-aa94-d07056733a1f    -                                       1bd1da76-b61f-db41-a2d2-c3474a31f38f    snapshots/2021-05-02T13:38:49Z_inf_btrfs_test
-"#)]
-        })};
-
-        assert_eq!(btrfs.get_local_subvolumes("test").unwrap(), vec![
-            Subvolume {uuid: Uuid::from_str("11eed410-7829-744e-8288-35c21d278f8e").unwrap(), path: String::from("/home"), parent_uuid: None, received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("32c672fa-d3ce-0b4e-8eaa-ab9205f377ca").unwrap(), path: String::from("/root"), parent_uuid: None, received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("5f0b151b-52e4-4445-aa94-d07056733a1f").unwrap(), path: String::from("/opt/btrfs_test"), parent_uuid: None, received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("7f305e3e-851b-974b-a476-e2f206e7a407").unwrap(), path: String::from("/snapshots/2021-05-02T07:40:32Z_inf_btrfs_test"), parent_uuid: Some(Uuid::from_str("5f0b151b-52e4-4445-aa94-d07056733a1f").unwrap()), received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("1bd1da76-b61f-db41-a2d2-c3474a31f38f").unwrap(), path: String::from("/snapshots/2021-05-02T13:38:49Z_inf_btrfs_test"), parent_uuid: Some(Uuid::from_str("5f0b151b-52e4-4445-aa94-d07056733a1f").unwrap()), received_uuid: None},
-        ]);
-    }
-
-    #[test]
-    fn get_remote_subvolumes() {
-        let mut btrfs = Btrfs { command: Box::new(CommandMock {
-            commands: vec![(String::from("sudo btrfs subvolume list -tupqR --sort=rootid /"), Context::Remote {host: "host".into(), user: "user".into(), identity: "/home/test/.ssh".into()})],
-            responses: vec![String::from(r#"ID      gen     parent  top level       parent_uuid     received_uuid   uuid    path
---      ---     ------  ---------       -----------     -------------   ----    ----
-256     10789   5       5               -                                       -                                       0b5cc138-af8e-2744-be4f-bdede1b509ef    root
-259     9051    256     256             -                                       -                                       574fef8d-7951-3e45-aa29-7167b9d4590a    var/lib/portables
-270     4965    256     256             -                                       dc4e1039-9241-cd47-9c10-a5d1ce15ba20    d1bd727c-8a02-bb44-bdd2-bae468651e98    backups/2021-05-04T19:48:42Z_inf_btrfs_test
-328     7505    256     256             19391f90-9007-3e4b-b757-6e5d2421b9bd    53bb5cfa-f45e-d147-9407-006271609062    54b52286-8265-9444-8603-214e7e0533e0    backups/2021-05-10T06:14:04Z_inf_btrfs_test
-"#)]
-        })};
-
-
-        assert_eq!(btrfs.get_remote_subvolumes("host", "user", "/home/test/.ssh").unwrap(), vec![
-            Subvolume {uuid: Uuid::from_str("0b5cc138-af8e-2744-be4f-bdede1b509ef").unwrap(), path: String::from("/root"), parent_uuid: None, received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("574fef8d-7951-3e45-aa29-7167b9d4590a").unwrap(), path: String::from("/var/lib/portables"), parent_uuid: None, received_uuid: None},
-            Subvolume {uuid: Uuid::from_str("d1bd727c-8a02-bb44-bdd2-bae468651e98").unwrap(), path: String::from("/backups/2021-05-04T19:48:42Z_inf_btrfs_test"), parent_uuid: None, received_uuid: Some(Uuid::from_str("dc4e1039-9241-cd47-9c10-a5d1ce15ba20").unwrap())},
-            Subvolume {uuid: Uuid::from_str("54b52286-8265-9444-8603-214e7e0533e0").unwrap(), path: String::from("/backups/2021-05-10T06:14:04Z_inf_btrfs_test"), parent_uuid: Some(Uuid::from_str("19391f90-9007-3e4b-b757-6e5d2421b9bd").unwrap()), received_uuid: Some(Uuid::from_str("53bb5cfa-f45e-d147-9407-006271609062").unwrap())},
-        ]);
     }
 }
--- src/command.rs
+++ src/command.rs
@@ -1,5 +1,7 @@
 use anyhow::{anyhow, Result, Context as _};
 use std::process;
+#[cfg(test)]
+mod tests;
 
 pub trait Command {
     /// Runs a command in the provided context
@@ -14,7 +16,7 @@ pub trait Command {
     /// * `commands` - a vector of tuples of command strings and contexts
     ///
     fn run_piped(&mut self, commands: &Vec<(&str, &Context)>) -> Result<String>;
-}
+}    
 
 #[derive(Debug, PartialEq)]
 pub enum Context {
@@ -30,7 +32,7 @@ pub enum Context {
     /// * `identity` - path and filename of the ssh identity file
     ///
     Remote { host: String, user: String, identity: String }
-}
+}    
 
 pub struct CommandSystem {}
 
@@ -42,18 +44,18 @@ impl CommandSystem {
             match child {
                 Some(mut c) => {
                     child = Some(self.run_single(command, context, Some(&mut c))?);
-                },
+                },    
                 None => {
                     child = Some(self.run_single(command, context, None)?);
-                }
-            }
-        }
+                }    
+            }    
+        }    
 
         let output = child.ok_or(anyhow!("error executing command"))?.wait_with_output()?;
         let output = check_output(&output).context("output of command to delete a snapshot contained an error")?;
 
         Ok(String::from_utf8(output)?)
-    }
+    }    
 
     fn run_single(&mut self, command: &str, context: &Context, pre: Option<&mut process::Child>) -> Result<process::Child> {
         let mut com = match context {
@@ -63,62 +65,40 @@ impl CommandSystem {
                     .arg(user)
                     .arg("bash")
                     .arg("-c");
-                com
-            },
+                com    
+            },    
             Context::Remote {host, user, identity} => {
                 let mut com = process::Command::new("ssh");
                 com.arg("-i")
                     .arg(identity)
                     .arg(format!("{}@{}", user, host));
-                com
-            }
-        };
+                com    
+            }    
+        };    
 
         match pre {
             Some(child) => {
                 let stdout = child.stdout.take().ok_or(anyhow!("error getting output of preceding command"))?;
                 com.stdin(stdout);
-            },
+            },    
             None => {}
-        }
+        }    
 
         com.stdout(process::Stdio::piped())
             .arg(command)
             .spawn().context("error executing command")
-    }
-}
+    }        
+}    
 
 impl Command for CommandSystem {
     fn run(&mut self, command: &str, context: &Context) -> Result<String> {
         self.run_piped(&vec!((command, context)))
-    }
+    }    
 
     fn run_piped(&mut self, commands: &Vec<(&str, &Context)>) -> Result<String> {
         self.run_piped(commands)
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::command::{Command, CommandSystem, Context};
-
-    #[test]
-    fn run() {
-        let mut com = CommandSystem {};
-
-        assert_eq!(com.run("ls Cargo.toml", &Context::Local{user: "hannes".into()}).unwrap(), "Cargo.toml\n");
-    }
-
-    #[test]
-    fn run_piped() {
-        let mut com = CommandSystem {};
-
-        assert_eq!(com.run_piped(&vec!(
-            ("cat Cargo.toml", &Context::Local{user: "hannes".into()}),
-            ("grep name", &Context::Local{user: "hannes".into()}),
-        )).unwrap(), "name = \"backup-local-rs\"\n");
-    }
-}
+    }    
+}    
 
 pub struct CommandMock {
     pub commands: Vec<(String, Context)>,
