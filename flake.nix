aa11d62a7dfd98ca2b386004f5581c743e9cbefd
Hannes Hochreiner <hannes@hochreiner.net>
2024-02-25 09:11:33 +0100

    change service not to be enabled

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
               systemd.services."hochreiner.backup-btrfs" = {
                 description = "backup-btrfs service";
                 wantedBy = [ "multi-user.target" ];
-
+                enable = false; # the service will be triggered by the timer
                 serviceConfig = let pkg = self.packages.${system}.default;
                 in {
                   Type = "oneshot";
